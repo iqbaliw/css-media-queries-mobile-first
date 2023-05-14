9ae5406f71ff8ee2a50b004727df520aa1063336
Iqbal Wahyudi <iqbalwahyudi20@gmail.com>
2023-05-15 06:03:07 +0700

    burger menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const ulMenu = document.querySelector("ul.menu");
+const toggleMenu = document.getElementById("showMenu");
+
+toggleMenu.addEventListener("click", function () {
+    ulMenu.classList.toggle("active");
+});
